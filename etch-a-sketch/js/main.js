8857698d65b59f6e7eaf55ff708facaca065908f
Mihai-Vladut <stoicavladut18@gmail.com>
2022-04-21 13:21:52 +0300

    Final update

--- etch-a-sketch/js/main.js
+++ etch-a-sketch/js/main.js
@@ -1,4 +1,5 @@
 const sketchContainer = document.getElementById("sketchContainer");
+const gridRangeLabel = document.getElementById("gridRangeLabel");
 const gridRange = document.getElementById("gridRange");
 
 const blackButton = document.getElementById("blackButton");
@@ -7,28 +8,17 @@ const rgbButton = document.getElementById("rgbButton");
 let gridSize = gridRange.value;
 const CONTAINER_SIZE = 500;
 
-let blackSelected = true;
-
-sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${CONTAINER_SIZE / gridSize}px)`;
-sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, ${CONTAINER_SIZE / gridSize}px)`;
+let blackSelected = null;
 
 gridRange.addEventListener("change", () => {
-  setGridSize();
-})
+  generateBlocks(gridRange.value);
 
-function setGridSize() {
-  removeBlocks();
-
-  gridSize = gridRange.value;
-
-  sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${CONTAINER_SIZE / gridSize}px)`;
-  sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, ${CONTAINER_SIZE / gridSize}px)`;
-
-  generateBlocks(gridSize);
-}
+  gridRangeLabel.textContent = gridRange.value;
+});
 
 function generateBlocks(gridSize) {
-  resetSketch();
+  removeBlocks();
+  setGridSize(gridSize);
 
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
@@ -39,37 +29,32 @@ function generateBlocks(gridSize) {
 
       sketchBlock.addEventListener("mouseover", () => {
         if (blackSelected) sketchBlock.style.background = "#000";
-        else sketchBlock.style.background = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        else sketchBlock.style.background = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
       });
     }
   }
 }
 
-function removeBlocks() {
-  sketchContainer.childNodes.forEach(block => {
-    block.remove();
-  });
+function setGridSize(gridSize) {
+  gridSize = gridRange.value;
+
+  sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${CONTAINER_SIZE / gridSize}px)`;
+  sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, ${CONTAINER_SIZE / gridSize}px)`;
 }
 
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    const sketchBlock = document.createElement("div");
-    sketchBlock.classList.add("sketch__block");
-
-    sketchContainer.append(sketchBlock);
-
-    sketchBlock.addEventListener("mouseover", () => {
-      if (blackSelected) sketchBlock.style.background = "#000";
-      else sketchBlock.style.background = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-    });
+function removeBlocks() {
+  // DO NOT USE FOREACH WHEN YOU WANT TO DELETE CHILD NODES BECAUSE `node.childNodes` IS A LIVE COLLECTION
+  // https://stackoverflow.com/questions/48310643/removing-childnodes-using-node-childnodes-foreach
+  while (sketchContainer.firstChild) {
+    sketchContainer.firstChild.remove();
   }
 }
 
-window.blackSketch = function () {
+window.blackSketch = function blackSketch() {
   resetSketch();
 
   blackButton.classList.add("button--is-selected");
-  rgbButton.classList.remove("button--is-selected");
+  if (rgbButton.classList.contains("button--is-selected")) rgbButton.classList.remove("button--is-selected");
 
   blackSelected = true;
 }
@@ -87,4 +72,10 @@ window.resetSketch = function resetSketch() {
   sketchContainer.childNodes.forEach(block => {
     block.style.background = "#d3d3d3";
   });
-}
+};
+
+// Invoke function itself
+(function () {
+  generateBlocks(gridSize);
+  blackSketch();
+})();
