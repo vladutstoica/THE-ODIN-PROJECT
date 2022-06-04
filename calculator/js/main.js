9fd6fc8355648243308fe1327b23fc7385ac6771
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-06-04 21:56:30 +0300

    Remake logic for operations

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -6,31 +6,33 @@ const equalInput = document.querySelectorAll("[data-equal]");
 
 let state = {
   first: {
-    param: "",
+    param: "", helper: "", isValid: false,
   },
+  operator: "",
   second: {
-    param: "",
+    param: "", helper: "", isValid: false,
   },
+  result: "",
 };
 
 numberInputs.forEach(number => {
   number.addEventListener("click", () => {
     console.log("Number pressed ", number.textContent);
-    updateStateArgs(number.textContent);
+    updateParam(number.textContent);
   })
 });
 
 operatorInputs.forEach(operator => {
   operator.addEventListener("click", () => {
     console.log("Operator pressed ", operator.textContent);
-    updateStateOperator(operator.textContent);
+    updateOperator(operator.textContent);
   })
 });
 
 helpersInputs.forEach(helper => {
   helper.addEventListener("click", () => {
-    updateStateHelpers(helper.textContent);
     console.log("Helper pressed ", helper.textContent);
+    switchHelpers(helper.textContent);
   })
 });
 
@@ -42,183 +44,142 @@ equalInput.forEach(equal => {
   });
 })
 
-function updateStateArgs(value) {
-  if (!state.operator) {
-    if (state.first.param === "0") state.first.param = "";
-    if (!state.first.helper) {
-      state.first.param += value;
-
-      state.first.valid = true;
-    } else if (!state.first.param.includes(state.first.helper)) {
-      state.first.param += state.first.helper;
-      state.first.helper = "";
-
-      state.first.valid = false;
-    } else {
-      state.first.param += value;
-      state.first.helper = "";
-
-      state.first.valid = true;
-    }
-
-    if (!state.first.valid) {
-      state.first.param = state.first.param + value;
-      state.first.valid = true
-    }
-
-    writeToOutput(state.first.param);
-  } else {
-    if (state.second.param === "0") state.second.param = "";
-
-    if (!state.second.helper) {
-      state.second.valid = true;
-      state.second.param += value;
-    } else if (!state.second.param.includes(state.second.helper)) {
-      state.second.param += state.second.helper;
-      state.second.helper = "";
-
-      state.second.valid = false;
-
-    } else {
-      state.second.param += value;
-      state.second.helper = "";
-
-      state.second.valid = true;
-    }
-    if (!state.second.valid) {
-      state.second.param = state.second.param + value;
-      state.second.valid = true
-    }
+function updateParam(value) {
+  // Update params with helper if it has one
+  if (state.result) {
+    state.second.param = "";
+    state.result = "";
+  }
+  if (state.operator) {
+    state.second.param += state.second.helper + value;
 
     writeToOutput(state.second.param);
+  } else {
+    state.first.param += state.first.helper + value;
+
+    writeToOutput(state.first.param);
   }
+
+  checkValid();
+
+
+  console.log(state.first, state.second);
 }
 
-function updateStateOperator(value) {
-  if (state.first.param && state.first.valid) state.operator = value;
-  state.first.helper = "";
-  state.second.helper = "";
+function updateOperator(value) {
+  if (state.result) state.first.param = state.result
+
+  state.operator = value;
 }
 
-function updateStateHelpers(value) {
-  state.second?.param ? state.second.helper = value : state.first.helper = value;
-
+function switchHelpers(value) {
   switch (value) {
-    case ".":
-      if (state.first.helper) return;
-      console.log(". status");
+    case "AC":
+      console.log(value);
       break;
     case "+/-":
-      if (state.first.helper) {
-        if (state.first.param > 0) {
-          state.first.param = "-" + state.first.param;
-
-          writeToOutput(state.first.param);
-        } else {
-          state.first.param = state.first.param.replace("-", "");
-
-          writeToOutput(state.first.param);
-
-        }
-
-        state.first.helper = "";
-      } else {
-        if (state.second.param > 0) {
-          state.second.param = "-" + state.second.param;
-
-          writeToOutput(state.second.param);
-
-        } else {
-          state.second.param = state.second.param.replace("-", "");
-
-          writeToOutput(state.second.param);
-
-        }
-
-        state.second.helper = "";
-      }
-
+      console.log(value);
       break;
-
-    case "AC":
-      if (state.second.param) {
-        state.second.param = "0";
-
-        writeToOutput(state.second.param);
-      } else {
-        state.first.param = "0";
-        state.operator = "";
-
-        writeToOutput(state.first.param);
-
-      }
-
-      state.first.helper = "";
-      state.second.helper = "";
-
-      console.log("AC status");
+    case ".":
+      dotCase(value);
       break;
   }
+}
+
+function dotCase(value) {
+  if (state.operator) state.second.helper = value; else state.first.helper = value;
+
+  if (state.first.param.includes(".") || state.second.param.includes(".")) {
+    state.first.helper = "";
+    state.second.helper = "";
+  }
+
+  // Add helper to the param
+  if (state.operator) {
+    state.second.param += state.second.helper;
+
+    writeToOutput(state.second.param);
+    checkValid();
+  } else {
+    state.first.param += state.first.helper;
+
+    writeToOutput(state.first.param);
+    checkValid();
+  }
+
+  // Delete helper after it was inserted in param
+  if (state.first.helper || state.second.helper) {
+    state.first.helper = "";
+    state.second.helper = "";
+  }
+}
+
+function checkValid() {
+  state.first.isValid = !(state.first.param.startsWith(".") || state.first.param.endsWith("."));
+  state.second.isValid = !(state.second.param.startsWith(".") || state.second.param.endsWith("."));
+
+  state.first.isValid = !(state.first.param === "");
+  state.second.isValid = !(state.second.param === "");
 }
 
 function calculateState() {
   switch (state.operator) {
     case "%":
-      remainder(parseInt(state.first.param), parseInt(state.second.param));
-
-      console.log(parseInt(state.first.param) % parseInt(state.second.param));
+      remainder(parseFloat(state.first.param), parseFloat(state.second.param));
       break;
     case "/":
-      divide(parseInt(state.first.param), parseInt(state.second.param));
-
-      console.log(parseInt(state.first.param) / parseInt(state.second.param));
+      divide(parseFloat(state.first.param), parseFloat(state.second.param));
       break;
     case "x":
-      multiply(parseInt(state.first.param), parseInt(state.second.param));
-
-      console.log(parseInt(state.first.param) * parseInt(state.second.param));
+      multiply(parseFloat(state.first.param), parseFloat(state.second.param));
       break;
     case "-":
-      subtract(parseInt(state.first.param), parseInt(state.second.param));
-
-      console.log(parseInt(state.first.param) - parseInt(state.second.param));
+      subtract(parseFloat(state.first.param), parseFloat(state.second.param));
       break;
     case "+":
-      add(parseInt(state.first.param), parseInt(state.second.param));
-
-      console.log(parseInt(state.first.param) + parseInt(state.second.param));
+      add(parseFloat(state.first.param), parseFloat(state.second.param));
       break;
   }
 }
 
 function add(n1, n2) {
-  let result = n1 + n2;
+  state.result = (n1 + n2).toString();
 
-  writeToOutput(result);
+  writeToOutput(state.result);
+
+  state.first.param = state.result;
 }
 
 function subtract(n1, n2) {
-  let result = n1 - n2;
+  state.result = (n1 - n2).toString();
 
-  writeToOutput(result);
+  writeToOutput(state.result);
+
+  state.first.param = state.result;
 }
 
 function multiply(n1, n2) {
-  let result = n1 * n2;
+  state.result = (n1 * n2).toString();
 
-  writeToOutput(result);
+  writeToOutput(state.result);
+
+  state.first.param = state.result;
 }
 
 function divide(n1, n2) {
-  let result = n1 / n2;
+  state.result = (n1 / n2).toString();
 
-  writeToOutput(result);
+  writeToOutput(state.result);
+
+  state.first.param = state.result;
 }
 
 function remainder(n1, n2) {
-  let result = n1 % n2;
+  state.result = n1 % n2;
 
-  writeToOutput(result);
+  writeToOutput(state.result);
+
+  state.first.param = state.result;
 }
 
 function writeToOutput(value) {
