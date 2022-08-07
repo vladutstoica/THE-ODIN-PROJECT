038f38618ce58d1ab06cddc8c4933157b940c358
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-08-07 22:57:30 +0300

    tic-tac-toe Implement <Square/> state

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,17 +1,23 @@
 import Square from "./Square";
+import {useState} from "react";
 
 export default function Board() {
+    const [squares, setSquares] = useState({array: Array(9).fill(null), xIsNext: true});
+
+    function handleClick(index) {
+        console.log(squares.array[index]);
+
+        squares.array[index] = squares.xIsNext ? "X" : "O";
+
+        setSquares({array: squares.array, xIsNext: !squares.xIsNext});
+        console.log(squares.array);
+    }
+
     return (
         <div className={"max-w-xs mx-auto grid grid-cols-3 gap-3 gap-y-5"}>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
+            {squares.array.map((item, index) => {
+                return <Square key={index} value={item} onClick={() => handleClick(index)}/>
+            })}
         </div>
     );
 }
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,13 +1,13 @@
-export default function Square() {
+import O from "./O";
+import X from "./X";
+
+export default function Square({value = "", onClick}) {
+
     return (
-        <button
-            className={"bg-custom-button shadow-[0_8px_rgba(0,0,0,0.2)] rounded-lg aspect-square active:translate-y-2 active:shadow-none"}>
-            <svg className={"w-12 mx-auto"} width="100" height="100" viewBox="0 0 100 100" fill="none"
-                 xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" clipRule="evenodd"
-                      d="M50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100ZM50 75C63.8071 75 75 63.8071 75 50C75 36.1929 63.8071 25 50 25C36.1929 25 25 36.1929 25 50C25 63.8071 36.1929 75 50 75Z"
-                      fill="#F2B237"/>
-            </svg>
+        <button onClick={onClick}
+                className={"bg-custom-button shadow-[0_8px_rgba(0,0,0,0.2)] rounded-lg aspect-square active:translate-y-2 active:shadow-none"}>
+            {value === "X" && <X/>}
+            {value === "O" && <O/>}
         </button>
     );
 }
