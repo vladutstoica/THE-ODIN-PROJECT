c776f6c13129458c6d3f54a1cc72643c3a69b46f
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-08-08 00:39:18 +0300

    tic-tac-toe Check for winner

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,16 +1,38 @@
 import Square from "./Square";
 import {useState} from "react";
 
+function checkWinner(gameStatus) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (const line of lines) {
+        const [a, b, c] = line;
+
+        if (gameStatus[a] && gameStatus[a] === gameStatus[b] && gameStatus[a] === gameStatus[c]) {
+            return gameStatus[a];
+        }
+    }
+
+    return null;
+}
+
 export default function Board() {
     const [squares, setSquares] = useState({array: Array(9).fill(null), xIsNext: true});
 
     function handleClick(index) {
-        console.log(squares.array[index]);
+        if (checkWinner(squares.array) || squares.array[index]) return;
 
         squares.array[index] = squares.xIsNext ? "X" : "O";
 
         setSquares({array: squares.array, xIsNext: !squares.xIsNext});
-        console.log(squares.array);
     }
 
     return (
